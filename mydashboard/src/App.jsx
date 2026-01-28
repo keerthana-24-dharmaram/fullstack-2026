9769f5392bfa935b80d87edf7bddb574074e5346
Keerthana Dharmaram <dharmaramkeerthana@gmail.com>
2026-01-28 16:40:20 +0530

    Add files via upload

---
+++ mydashboard/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Dashboard</h1>
+      <p>This dashboard displays course information.</p>
+
+      <h2>Courses</h2>
+      <ul>
+        <li>Full Stack Development</li>
+        <li>React Basics</li>
+        <li>Machine Learning</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
