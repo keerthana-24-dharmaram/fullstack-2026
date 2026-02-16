9a4c3cbfda3652b060dd2da81114ed5c64a36204
Keerthana Dharmaram <dharmaramkeerthana@gmail.com>
2026-02-16 10:47:37 +0530

    Added frontend as normal folder

---
+++ Personal-finance-tracker1/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from './Dashboard'; // Import the Dashboard component
+
+const App = () => {
+  return (
+    <div className="App">
+      <Dashboard />  {/* Use the Dashboard component here */}
+    </div>
+  );
+};
+
+export default App;
